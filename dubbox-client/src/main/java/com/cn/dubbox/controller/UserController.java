964db0d862e6b8bb37fd5595a97c2c08aac9e378
yzhming <yzhmingming@163.com>
2018-11-14 16:47:53 +0800

    测试成功

--- dubbox-client/src/main/java/com/cn/dubbox/controller/UserController.java
+++ dubbox-client/src/main/java/com/cn/dubbox/controller/UserController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.cn.dubbox.service.UserService;
 
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	@Reference
 	private UserService userService;
 	
 	@RequestMapping("/showName")
